fd8edf27ef14eb470f5fe9f2824d28d56574a1cd
Vlad <filbertby@gmail.com>
2023-10-09 19:14:30 +0200

    max speed

--- ppm2ibus.ino/ppm2ibus.ino.ino
+++ ppm2ibus.ino/ppm2ibus.ino.ino
@@ -17,9 +17,12 @@
 volatile uint16_t ppm_channel_data[PPM_MAX_CHANNELS] = { 0 };
 volatile uint8_t  ppm_pulse_index   = 0;
 volatile uint8_t  ppm_channel_count = PPM_MAX_CHANNELS;
+volatile uint8_t  ppm_num_off_channels = 0;
 
 byte serial_buffer[IBUS_FRAME_LENGTH] = { 0 };
 int buffer_index = 0;
+
+bool hasBeenUpdated = false;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -32,6 +35,11 @@ void setup() {
 
 void WriteSerial()
 {
+  // whlte ready flag
+  if (!hasBeenUpdated) 
+    return;
+  hasBeenUpdated = false;
+
 	uint16_t ibus_checksum = ((uint16_t)0xFFFF);
   buffer_index = 0;
 
@@ -76,7 +84,7 @@ void loop() {
 	WriteSerial();
 
 	// Delay before next update
-	delay(10);
+	//delay(10);
 }
 
 ISR(PCINT0_vect) {
@@ -89,10 +97,13 @@ ISR(PCINT0_vect) {
     if (0 != lastTimeStamp) {
         uint16_t width = (uint16_t)(currentTimeStamp - lastTimeStamp);
         if(width > PPM_LOW_SEPARATOR){
+          ppm_num_off_channels = ppm_pulse_index;
           ppm_pulse_index = 0;
         }
         else {
           ppm_channel_data[ppm_pulse_index++] = width;
+          if(ppm_num_off_channels == ppm_pulse_index)
+            hasBeenUpdated = true;
         }
       }
     lastTimeStamp = currentTimeStamp;
